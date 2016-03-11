2fbad26198c8db10355b9e1e334ea1f2c3730a2d
Suresh Babu Kumpati <sureshkumpati@gmail.com>
2016-03-12 01:13:24 +0530

    plot3.R

---
+++ plot3.R
@@ -0,0 +1,14 @@
+## Coursera - Exploratory Data Analysis - Plotting Assignment 1
+##
+## plot3.R - generates plot3.png
+
+## Generating Plot 3
+
+with(data, {
+  plot(Sub_metering_1 ~ Datetime, type = "l", 
+       ylab = "Global Active Power (kilowatts)", xlab = "")
+  lines(Sub_metering_2 ~ Datetime, col = 'Red')
+  lines(Sub_metering_3 ~ Datetime, col = 'Blue')
+})
+legend("topright", col = c("black", "red", "blue"), lty = 1, lwd = 2, 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
