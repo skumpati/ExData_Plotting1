8aa145aa729ed2a1bc0d6fd2305022070c7448fd
Suresh Babu Kumpati <sureshkumpati@gmail.com>
2016-03-12 00:55:27 +0530

    plot1.R

--- plot1.R
+++ plot1.R
@@ -8,6 +8,6 @@ fh <- file("household_power_consumption.txt")
 ba <- read.table(text = grep("^[1,2]/2/2007", readLines(fh), value = TRUE), col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep = ";", header = TRUE)
 hist(ba$Global_active_power, col = "red", main = paste("Global Active Power"), xlab = "Global Active Power (kilowatts)")
 
-## Creating or generating the plot
+## Creating or generating the plot, Note: the PNG of 480 pixels width and Height can be set whilst saving the plot via. RStudio
 hist(ba$Global_active_power, col = "red", main = paste("Global Active Power"), xlab = "Global Active Power (kilowatts)")
 
